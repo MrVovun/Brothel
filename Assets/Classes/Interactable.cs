5fc86f1bf1e60b26efec1994190f74a6ec9fbc7c
Vovun <mrvovun@gmail.com>
2019-02-20 10:36:20 +0200

    Movement and interaction

---
+++ Assets/Classes/Interactable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float interactionRadius = 3f;
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Vector3 offset;
+    public float pitch = 2f;
+
+    public void Focus (Transform target) {
+        transform.position = target.position - offset;
+        Debug.Log ("Camera focused on: " + target);
+    }
+
+}
