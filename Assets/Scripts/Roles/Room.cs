a0314b7f77c6b3e19c511cb2ce3f6e932f5930e0
Vovun <mrvovun@gmail.com>
2019-03-12 14:33:54 +0200

    Fix room cleaning from client

--- Assets/Scripts/Roles/Room.cs
+++ Assets/Scripts/Roles/Room.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 
 public class Room : MonoBehaviour {
     public Transform Bed;
-    
+
     private bool isOccupied;
 
-    public Vector3 GetBedPostition() {
+    public Vector3 GetBedPostition () {
         return Bed.position;
     }
 
-    public void Occupy(Whore whore, Client client) {
+    public void Occupy (Whore whore, Client client) {
         isOccupied = true;
     }
 
-    public bool IsOccupied() {
+    public bool IsOccupied () {
         return isOccupied;
+    }
+
+    public void Unoccupy () {
+        isOccupied = false;
     }
 }
--- Assets/Scripts/Roles/Whore.cs
+++ Assets/Scripts/Roles/Whore.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Walker))]
+[RequireComponent (typeof (Walker))]
 public class Whore : Interactable {
     [Space]
     public WhoreData Personality;
@@ -12,51 +12,52 @@ public class Whore : Interactable {
     private Coroutine handlingClientProcess;
     private bool isBusy;
 
-    private void OnEnable() {
-        walker = GetComponent<Walker>();
+    private void OnEnable () {
+        walker = GetComponent<Walker> ();
         returnPoint = transform.position;
     }
 
-    public void MeetNewClient(Client client) {
-        GetComponent<Walker>().WalkToTarget(client);
+    public void MeetNewClient (Client client) {
+        GetComponent<Walker> ().WalkToTarget (client);
     }
 
-    public bool IsBusy() {
+    public bool IsBusy () {
         return isBusy;
     }
 
-    public void HandleClient(Client client) {
-        handlingClientProcess = StartCoroutine(handlingClient(client));
+    public void HandleClient (Client client) {
+        handlingClientProcess = StartCoroutine (handlingClient (client));
     }
 
-    private IEnumerator handlingClient(Client client) {
+    private IEnumerator handlingClient (Client client) {
         isBusy = true;
-        
-        Room room = RoomManager.Instance.FindRoom();
+
+        Room room = RoomManager.Instance.FindRoom ();
         if (room) {
-            Walker walker = GetComponent<Walker>();
-            walker.GoToPoint(room.GetBedPostition());
-            room.Occupy(this, client);
-            
-            client.DoWhore(this);
-            
-            yield return new WaitUntil(() => walker.HasArrived());
-            Debug.Log("Hello Darling! (couple arrived to room)");
-            yield return new WaitForSeconds(30);
-            client.Handled(this);
-            Handled(client);  
+            Walker walker = GetComponent<Walker> ();
+            walker.GoToPoint (room.GetBedPostition ());
+            room.Occupy (this, client);
+
+            client.DoWhore (this);
+
+            yield return new WaitUntil (() => walker.HasArrived ());
+            Debug.Log ("Hello Darling! (couple arrived to room)");
+            yield return new WaitForSeconds (30);
+            client.Handled (this);
+            Handled (client);
+            room.Unoccupy ();
         } else {
-            Debug.LogWarning("There is no free room!");
+            Debug.LogWarning ("There is no free room!");
         }
     }
 
-    public void Handled(Client client) {
+    public void Handled (Client client) {
         isBusy = false;
-        GoBack();
+        GoBack ();
         handlingClientProcess = null;
     }
 
-    public void GoBack() {
-        walker.GoToPoint(returnPoint);
+    public void GoBack () {
+        walker.GoToPoint (returnPoint);
     }
 }
