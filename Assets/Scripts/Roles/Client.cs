014c4b8ca97bd5c6512408c122f9371b7cb4cda1
Vovun <mrvovun@gmail.com>
2019-03-26 18:12:43 +0200

    Basic client payment

--- Assets/Scripts/Roles/Client.cs
+++ Assets/Scripts/Roles/Client.cs
@@ -7,6 +7,7 @@ public class Client : Interactable {
     public int staminaRequired;
     public int level;
     public int expForMe;
+    public int amountClientWillPay;
 
     private bool isBusy;
     private Walker walker;
@@ -35,6 +36,23 @@ public class Client : Interactable {
     public void Handled (Whore whore) {
         isBusy = false;
         Debug.Log ("Client has been handled");
+        //payment
         Destroy (gameObject);
     }
+
+    public void Interrupted (Whore whore) {
+        isBusy = false;
+        Debug.Log ("Sex was interrupted");
+        Destroy (gameObject);
+    }
+
+    public int GeneratePayment (Whore whore) {
+        int reqTime = 50;
+        int needsSatisfied = 50;
+        int payPerNeed = 50;
+        int payPerFetish = 50;
+        amountClientWillPay = reqTime + payPerNeed * needsSatisfied + payPerFetish;
+        return amountClientWillPay;
+    }
+
 }
--- Assets/Scripts/UI/WhoreSelectionUI.cs
+++ Assets/Scripts/UI/WhoreSelectionUI.cs
@@ -71,28 +71,4 @@ public class WhoreSelectionUI : MonoBehaviour {
         }
         buttons.Clear ();
     }
-    //
-    //    private void Awake() {
-    //        GetComponent<Button>().onClick.AddListener(delegate {
-    ////            GameController.Instance.ShowInfo(thisWhoreInfo, isGenericInfo);
-    //        });
-    //    }
-    //
-    //    public void FitsToClient() {
-    //        GetComponentInChildren<TextMeshProUGUI>().text = GetComponentInChildren<TextMeshProUGUI>().text + "\nFits!";
-    //    }
-
-    //
-    //    public void AssignWhore(GameObject whorePrefab) {
-    //        whoreGen = whorePrefab.GetComponent<Whore>();
-    ////        whoreGen.thisWhoreButton = gameObject;
-    //        GetComponent<Image>().sprite = whoreGen.Personality.whorePortrait;
-    //        GetComponentInChildren<TextMeshProUGUI>().text = whoreGen.Personality.whoreName;
-    ////        if (GameController.Instance.CompareClient().Contains(whoreGen.WhoreData) == true) {
-    ////            FitsToClient();
-    ////        }
-    //
-    //        thisWhoreInfo = "Name: " + whoreGen.Personality.whoreName + "\n" + "Description: " + whoreGen.Personality.whoreDesc + "\n" +
-    //                        "Fits to client: " + whoreGen.Personality.fitsToClient;
-    //    }
 }
