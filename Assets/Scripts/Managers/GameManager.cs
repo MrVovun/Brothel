481b99da3dca77f30c7c4698c06388ab8f7ef6e4
Vovun <mrvovun@gmail.com>
2019-04-23 18:06:53 +0200

    Text manager, text block in UI, game pause on P

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,12 +3,24 @@
 public class GameManager : MonoBehaviourSingleton<GameManager> {
 
     public int money;
+    private bool isPaused = false;
     private void Start () {
         DataFactory.Instance.GatherNames ();
     }
     void Update () {
         if (Input.GetKeyDown (KeyCode.Space)) {
             ClientManager.Instance.SpawnClient ();
+        }
+        if (Input.GetKeyDown (KeyCode.Q)) {
+            TextManager.Instance.Say ("Do you suck dicks?", null, true);
+        }
+        if (Input.GetKeyDown (KeyCode.P)) {
+            if (isPaused == true) {
+                isPaused = false;
+            } else {
+                isPaused = true;
+            }
+
         }
     }
 }
---
+++ Assets/Scripts/Managers/TextManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviourSingleton<TextManager> {
+
+    private string fullString;
+    private TextMeshProUGUI myText;
+    private Sprite currentLeftPic;
+    private Sprite currentRightPic;
+
+    [SerializeField]
+    private float charTime = 0.1f;
+
+    [SerializeField]
+    private GameObject textHolder;
+
+    private void Start () {
+        myText = textHolder.GetComponentInChildren<TextMeshProUGUI> ();
+    }
+
+    public void Say (string message, Sprite speakerSprite, bool isClient) {
+        if (isClient == true) {
+            speakerSprite = currentRightPic;
+        } else {
+            speakerSprite = currentLeftPic;
+        }
+        fullString = message;
+        textHolder.SetActive (true);
+        StartCoroutine (ShowText ());
+    }
+
+    IEnumerator ShowText () {
+        myText.text = string.Empty;
+        foreach (char c in fullString) {
+            if (Input.GetKeyDown (KeyCode.Mouse0)) {
+                myText.text = fullString;
+                yield break;
+            }
+            myText.text += c;
+            yield return new WaitForSeconds (charTime);
+        }
+    }
+
+    public void StopSpeaking () {
+        textHolder.SetActive (false);
+        myText.text = string.Empty;
+    }
+
+    //scroll back/forth the text
+    //pause the game
+}
